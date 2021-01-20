f6fe2eada9713ff2b4056d37f6cf4adb09c25930
Jakub Szymczak <jakubator@gmail.com>
2021-01-20 23:47:57 +0000

    Init of GetUserCarbonHandler

---
+++ src/main/java/com/thinktech/GetUserCarbonHandler.java
@@ -0,0 +1,36 @@
+package com.thinktech;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thinktech.model.*;
+import com.thinktech.model.enums.CarbonSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GetUserCarbonHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
+    {
+        private static final Logger LOG = LogManager.getLogger(CalculateInitialCarbonHandler.class);
+
+        @Override
+        public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+            LOG.info("received request");
+
+            String userId = request.getPathParameters().get("userId");
+
+
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+            response.setStatusCode(200);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                String responseBody = objectMapper.writeValueAsString(carbonForDate);
+                response.setBody(responseBody);
+            }
+            catch (JsonProcessingException e) {
+                LOG.error("Unable to convert result to JSON", e);
+            }
+            return response;
+}
