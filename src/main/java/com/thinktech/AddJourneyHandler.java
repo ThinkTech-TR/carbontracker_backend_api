af6f8cdf2d6c0323e42b148cabdabb8bc96137fa
Zap-Master <kim.lidiya2011@gmail.com>
2021-02-03 07:51:48 +0000

    refactored and call a store procedure for adding new journey

--- src/main/java/com/thinktech/AddJourneyHandler.java
+++ src/main/java/com/thinktech/AddJourneyHandler.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thinktech.model.domain.DataForTrackingPage;
+import com.thinktech.service.database.TrackingDataProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,9 +25,6 @@ import java.util.*;
 public class AddJourneyHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
 {
     private static final Logger LOG = LogManager.getLogger(AddJourneyHandler.class);
-    private Connection connection = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
@@ -37,71 +35,30 @@ public class AddJourneyHandler implements RequestHandler<APIGatewayProxyRequestE
         LocalDate finishDate = LocalDate.parse(request.getPathParameters().get("finishDate"), formatter_1);
         String requestBody = request.getBody();
 
-        ObjectMapper objMapper = new ObjectMapper();
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(200);
+        ObjectMapper objMapper = new ObjectMapper();
         Map<String, String> headers = new HashMap<>();
         headers.put("Access-Control-Allow-Origin", "*");
         headers.put("Access-Control-Allow-Credentials", "true");
         response.setHeaders(headers);
+        String responseBody = "Unable to add new journey";
+        response.setBody(responseBody);
+        response.setStatusCode(500);
 
         try {
-            System.out.println(requestBody);
             DataForTrackingPage journey = objMapper.readValue(requestBody, DataForTrackingPage.class);
-            //get new journey_id from table Journey
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s&useSSL=false",
-                        System.getenv("DB_HOST"),
-                        System.getenv("DB_NAME"),
-                        System.getenv("DB_USER"),
-                        System.getenv("DB_PASSWORD")));
-                preparedStatement = connection.prepareStatement("SELECT max(j.journey_id)+1 as journey_id FROM carbon.journey j");
-
-                resultSet = preparedStatement.executeQuery();
-                resultSet.first();
-                journey.setIdJourney(resultSet.getInt("journey_id"));
-            }
-            catch (Exception e) {
-                LOG.error(String.format("Unable to get next journey_id from Journey table for user %s", userId), e);
-            }
-            finally {
-                closeConnection();
-            }
-            //toDo calculate Carbon
-            journey.setEmission(0.8);
-            //insert to table Journey
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s&useSSL=false",
-                        System.getenv("DB_HOST"),
-                        System.getenv("DB_NAME"),
-                        System.getenv("DB_USER"),
-                        System.getenv("DB_PASSWORD")));
-                preparedStatement = connection.prepareStatement("insert into carbon.journey (user_id, transport_id, journey_date, distance_miles, journey_id) " +
-                        "values (?, ?, ?, ?, ?);");
-                preparedStatement.setInt(1, Integer.parseInt(userId));
-                preparedStatement.setInt(2, journey.getTrackingItemId());
-                preparedStatement.setDate(3, java.sql.Date.valueOf(journey.getTrackingDate()));
-                preparedStatement.setInt(4, journey.getDistance());
-                preparedStatement.setInt(5, journey.getIdJourney());
-                preparedStatement.execute();
-            }
-            catch (Exception e) {
-                LOG.error(String.format("Unable to insert data into Journey table  for user %s", userId), e);
-            }
-            finally {
-                closeConnection();
-            }
-            LOG.debug("Saved journey " + journey.getTrackingItemName() + " date " + journey.getTrackingDate());
-
-            try {
-                String responseBody = objMapper.writeValueAsString(journey);
+                TrackingDataProvider provider = new TrackingDataProvider();
+                provider.AddJourney(journey);
+                responseBody = objMapper.writeValueAsString(journey);
                 response.setBody(responseBody);
+                response.setStatusCode(200);
+                LOG.debug("Saved journey " + journey.getTrackingItemName() + " date " + journey.getTrackingDate());
             }
-            catch (JsonProcessingException e) {
-                LOG.error("Unable to convert result to JSON", e);
+            catch (Exception e){
+                LOG.error("Error processing request", e);
             }
+
         } catch (JsonParseException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
@@ -109,22 +66,4 @@ public class AddJourneyHandler implements RequestHandler<APIGatewayProxyRequestE
         }
         return response;
     }
-
-    private void closeConnection() {
-        try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        }
-        catch (Exception e) {
-            LOG.error("Unable to close connections to MySQL - {}", e.getMessage());
-        }
-    }
-
 }
---
+++ src/main/java/com/thinktech/service/database/TrackingDataProvider.java
@@ -0,0 +1,29 @@
+package com.thinktech.service.database;
+
+import com.thinktech.model.domain.DataForTrackingPage;
+
+public class TrackingDataProvider extends CarbonDataProvider {
+    public void AddJourney(DataForTrackingPage journey) throws Exception {
+        try {
+            connection = this.OpenConnection();
+
+            callableStatement = connection.prepareCall("{CALL ADD_JOURNEY(?, ?, ?, ?, ?)}");
+            callableStatement.setString("p_auth_user_id", journey.getAuthUserId());
+            callableStatement.setInt("p_transport_id", journey.getTrackingItemId());
+            callableStatement.setDate("p_journey_date", java.sql.Date.valueOf(journey.getTrackingDate()));
+            callableStatement.setInt("p_distance_miles", journey.getDistance());
+            callableStatement.setInt("p_journey_id", 0);
+
+            callableStatement.executeUpdate();
+
+            journey.setIdJourney(callableStatement.getInt("p_journey_id"));
+            System.out.println(journey);
+
+        } catch (Exception e) {
+            String message = String.format("Unable to add user to database %s", journey.getAuthUserId());
+            throw new Exception(message, e);
+        } finally {
+            CloseConnection();
+        }
+    }
+}
