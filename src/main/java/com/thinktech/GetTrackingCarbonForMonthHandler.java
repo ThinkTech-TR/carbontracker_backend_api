2ed1dc92a6293c434e11c7434f572248c089dff6
Zap-Master <kim.lidiya2011@gmail.com>
2021-02-05 21:34:29 +0000

    added kg_co2 from database

--- src/main/java/com/thinktech/GetTrackingCarbonForMonthHandler.java
+++ src/main/java/com/thinktech/GetTrackingCarbonForMonthHandler.java
@@ -7,10 +7,10 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thinktech.model.domain.DataForTrackingPage;
+import com.thinktech.service.database.TrackingDataProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
@@ -47,149 +47,21 @@ public class GetTrackingCarbonForMonthHandler implements RequestHandler<APIGatew
 
         //get data from table Questionnaire
         List<DataForTrackingPage> journeys = new ArrayList<>();
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
-                    System.getenv("DB_HOST"),
-                    System.getenv("DB_NAME"),
-                    System.getenv("DB_USER"),
-                    System.getenv("DB_PASSWORD")));
-            preparedStatement = connection.prepareStatement("SELECT q.carMileage,   " +
-                    "q.carUsage, " +
-                    "        q.diet, " +
-                    "        q.houseAge, " +
-                    "        q.houseType, " +
-                    "        q.numberInHousehold, " +
-                    "        q.questionaire_id, " +
-                    "        u.auth_user_id, " +
-                    "        q.userCategory " +
-                    "FROM carbon.questionaire as q, carbon.users u " +
-                    "WHERE q.user_id = u.user_id "+
-                    "       and u.auth_user_id = ?");
-            preparedStatement.setString(1, userId);
-            resultSet = preparedStatement.executeQuery();
 
+        TrackingDataProvider provider = new TrackingDataProvider();
+            try {
+                List<DataForTrackingPage> itemsFromQuestionnaire = provider.DataFromQuestionnaire(userId, amountOfDaysInPeriod, startDate);
+                List<DataForTrackingPage> itemsFromJourney = provider.DataFromJourney(userId,amountOfDaysInPeriod, sqlStartDate, sqlFinishDate);
 
-            int counter = 1;
-            while (resultSet.next()) {
-                String strTrackingNameHousing = "";
-                double emissionHousing = 0;
-                String strTrackingNameCar = "";
-                double emissionCar = 0;
-                int mileage = 0;
-                String strTrackingNameDiet = "";
-                double emissionDiet = 0;
-                if (resultSet.getString("houseType") != null) {
-                    strTrackingNameHousing = "Housing";
-                    emissionHousing = 4.0;
-                }
-                if (resultSet.getString("carUsage") != null) {
-                    strTrackingNameCar = "car";
-                    mileage = Math.round(resultSet.getInt("carMileage")/365);
-                    emissionCar = 2.0;
-                }
-                if (resultSet.getString("diet") != null) {
-                    strTrackingNameDiet = resultSet.getString("diet");
-                    emissionDiet = 0.5;
-                }
-                counter ++;
-                for (int i = 0; i < amountOfDaysInPeriod; i++){
-                    DataForTrackingPage house = new DataForTrackingPage(
-                            0,
-                            strTrackingNameHousing,
-                            0,
-                            emissionHousing,
-                            false,
-                            startDate.plusDays(i).toString() ,
-                            counter,
-                            0,
-                            resultSet.getString("auth_user_id"));
-                    journeys.add(house);
-                    counter ++;
-                    if (emissionCar != 0) {
-                        DataForTrackingPage car = new DataForTrackingPage(0,
-                                strTrackingNameCar,
-                                mileage,
-                                emissionCar,
-                                true,
-                                startDate.plusDays(i).toString(),
-                                counter,
-                                0,
-                                resultSet.getString("auth_user_id"));
-                        journeys.add(car);
-                        counter++;
-                    }
-                    DataForTrackingPage diet = new DataForTrackingPage(0,
-                            strTrackingNameDiet,
-                            0,
-                            emissionDiet,
-                            false,
-                            startDate.plusDays(i).toString() ,
-                            counter,
-                            0,
-                            resultSet.getString("auth_user_id"));
-                    journeys.add(diet);
-                    counter ++;
-                }
-            }
-            resultSet.close();
-            preparedStatement.close();
-            connection.close();
-
-
-
-            //get data from table Journey
-            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
-                    System.getenv("DB_HOST"),
-                    System.getenv("DB_NAME"),
-                    System.getenv("DB_USER"),
-                    System.getenv("DB_PASSWORD")));
-            preparedStatement = connection.prepareStatement("SELECT j.distance_miles, " +
-                    " j.journey_date, " +
-                    " j.journey_id, " +
-                    " j.transport_id, " +
-                    " t.transport_type, " +
-                    " u.auth_user_id " +
-                    "FROM carbon.journey as j, carbon.transport t, carbon.users u  " +
-                    "WHERE j.user_id = u.user_id " +
-                    "and j.journey_date <= ? " +
-                    "and j.journey_date >= ? " +
-                    "and t.transport_id = j.transport_id " +
-                    "and u.auth_user_id = ?" +
-                    "ORDER BY j.journey_date");
-            preparedStatement.setDate(1,sqlFinishDate);
-            preparedStatement.setDate(2,sqlStartDate);
-            preparedStatement.setString(3, userId);
-            resultSet = preparedStatement.executeQuery();
-            counter ++;
-            while (resultSet.next()) {
-                //toDo calculate emission
-                double emission = 3.5;
-                DataForTrackingPage journey = new DataForTrackingPage(resultSet.getInt("transport_id"),
-                        resultSet.getNString("transport_type"),
-                        resultSet.getInt("distance_miles"),
-                        emission,
-                        true,
-                        resultSet.getString("journey_date"),
-                        counter,
-                        resultSet.getInt("journey_id"),
-                        resultSet.getString("auth_user_id"));
-                journeys.add(journey);
-                counter ++;
-            }
-            }
-            catch (Exception e) {
-                LOG.error(String.format("Unable to query database for tasks for user %s", userId), e);
-            }
-            finally {
-                closeConnection();
+                journeys.addAll(itemsFromQuestionnaire);
+                journeys.addAll(itemsFromJourney);
+            } catch (Exception e) {
+                LOG.error("Error processing request", e);
             }
         //sort journeys by date
         journeys.sort(Comparator.comparing(DataForTrackingPage::getTrackingDate).reversed());
 
         //toDo delete car-journey (based on Questionnaire)  if there is a real journey based on Journey table for specific date
-
-
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         response.setStatusCode(200);
         Map<String, String> headers = new HashMap<>();
@@ -205,22 +77,5 @@ public class GetTrackingCarbonForMonthHandler implements RequestHandler<APIGatew
             LOG.error("Unable to convert result to JSON", e);
         }
         return response;
-    }
-
-    private void closeConnection() {
-        try {
-                 if (resultSet != null) {
-                    resultSet.close();
-                 }
-                 if (preparedStatement != null) {
-                     preparedStatement.close();
-                 }
-                 if (connection != null) {
-                     connection.close();
-                 }
-            }
-        catch (Exception e) {
-            LOG.error("Unable to close connections to MySQL - {}", e.getMessage());
-        }
     }
 }
--- src/main/java/com/thinktech/service/database/TrackingDataProvider.java
+++ src/main/java/com/thinktech/service/database/TrackingDataProvider.java
@@ -2,6 +2,10 @@ package com.thinktech.service.database;
 
 import com.thinktech.model.domain.DataForTrackingPage;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrackingDataProvider extends CarbonDataProvider {
     public void AddJourney(DataForTrackingPage journey) throws Exception {
         try {
@@ -36,7 +40,7 @@ public class TrackingDataProvider extends CarbonDataProvider {
             callableStatement.setDate("p_journey_date", java.sql.Date.valueOf(journey.getTrackingDate()));
             callableStatement.setInt("p_distance_miles", journey.getDistance());
             callableStatement.setInt("p_journey_id", journey.getIdJourney());
-            System.out.println(callableStatement);
+            //System.out.println(callableStatement);
             callableStatement.executeUpdate();
 
         } catch (Exception e) {
@@ -47,4 +51,121 @@ public class TrackingDataProvider extends CarbonDataProvider {
         }
     }
 
+    public List<DataForTrackingPage> DataFromQuestionnaire (String authUserId, long amountOfDaysInPeriod, LocalDate startDate) throws Exception {
+        List<DataForTrackingPage> items = new ArrayList<>();
+        try {
+            connection = this.OpenConnection();
+            preparedStatement = connection.prepareStatement("SELECT q.carMileage, " +
+                    "q.carUsage, " +
+                    "q.diet, " +
+                    "q.houseAge, " +
+                    "q.houseType, " +
+                    "q.numberInHousehold, " +
+                    "q.questionaire_id, " +
+                    "q.user_id, " +
+                    "q.userCategory, " +
+                    "car.kg_carbon_per_mile car_carbon_per_mile, " +
+                    "h.kg_carbon_annual house_carbon_annual, " +
+                    "d.kg_carbon_annual diet_carbon_annual " +
+                    "FROM carbon.questionaire as q " +
+                    "LEFT JOIN carbon.car_carbon car ON car.car_type = q.carUsage " +
+                    "LEFT JOIN carbon.housing h ON (h.housing_age = q.houseAge and h.housing_type = q.houseType) " +
+                    "LEFT JOIN carbon.diet_carbon d ON (d.diet_type = q.diet) " +
+                    "INNER join carbon.users u ON  (q.user_id = u.user_id  and u.auth_user_id = ?)");
+            preparedStatement.setString(1, authUserId);
+            //System.out.println(preparedStatement);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.first();
+            int counter = 1;
+            for (int i = 0; i < amountOfDaysInPeriod; i++){
+                    DataForTrackingPage house = new DataForTrackingPage(0,
+                            "House",
+                            0,
+                            Math.round(100.00 * resultSet.getDouble("house_carbon_annual")/365) / 100.00,
+                            false,
+                            startDate.plusDays(i).toString() ,
+                            counter,
+                            0,
+                            authUserId);
+                    items.add(house);
+                    counter ++;
+                    if (resultSet.getInt("carMileage") != 0) {
+                        DataForTrackingPage car = new DataForTrackingPage(0,
+                                "Car",
+                                Math.round(100 * resultSet.getInt("carMileage") / 365) /100,
+                                Math.round(100.00 * resultSet.getDouble("car_carbon_per_mile") * resultSet.getInt("carMileage") / 365) /100.00,
+                                true,
+                                startDate.plusDays(i).toString(),
+                                counter,
+                                0,
+                                authUserId);
+                        items.add(car);
+                        counter++;
+                    }
+                    DataForTrackingPage diet = new DataForTrackingPage(0,
+                            "Diet",
+                            0,
+                            Math.round(100.00 * resultSet.getInt("diet_carbon_annual") / 365) / 100.00,
+                            false,
+                            startDate.plusDays(i).toString() ,
+                            counter,
+                            0,
+                            authUserId);
+                    items.add(diet);
+                    counter ++;
+            }
+            return items;
+        } catch (Exception e) {
+            String message = String.format("Unable to query database to check user %s", authUserId);
+            throw new Exception(message, e);
+        } finally {
+            CloseConnection();
+        }
+    }
+
+    public List<DataForTrackingPage> DataFromJourney (String authUserId, long amountOfDaysInPeriod,
+                                                      java.sql.Date sqlStartDate, java.sql.Date sqlFinishDate) throws Exception {
+        List<DataForTrackingPage> items = new ArrayList<>();
+        try {
+            connection = this.OpenConnection();
+            preparedStatement = connection.prepareStatement("SELECT j.distance_miles, " +
+                    " j.journey_date, " +
+                    " j.journey_id, " +
+                    " j.transport_id, " +
+                    " t.transport_type, " +
+                    "t.kg_carbon_per_mile, " +
+                    " u.auth_user_id " +
+                    "FROM carbon.journey as j, carbon.transport t, carbon.users u  " +
+                    "WHERE j.user_id = u.user_id " +
+                    "and j.journey_date <= ? " +
+                    "and j.journey_date >= ? " +
+                    "and t.transport_id = j.transport_id " +
+                    "and u.auth_user_id = ?" +
+                    "ORDER BY j.journey_date");
+            preparedStatement.setDate(1,sqlFinishDate);
+            preparedStatement.setDate(2,sqlStartDate);
+            preparedStatement.setString(3, authUserId);
+            resultSet = preparedStatement.executeQuery();
+            int counter = 2021;
+            while (resultSet.next()) {
+                DataForTrackingPage journey = new DataForTrackingPage(resultSet.getInt("transport_id"),
+                        resultSet.getNString("transport_type"),
+                        resultSet.getInt("distance_miles"),
+                        Math.round(100.00 * resultSet.getDouble("kg_carbon_per_mile") * resultSet.getInt("distance_miles") )/ 100.00,
+                        true,
+                        resultSet.getString("journey_date"),
+                        counter,
+                        resultSet.getInt("journey_id"),
+                        resultSet.getString("auth_user_id"));
+                items.add(journey);
+                counter ++;
+            }
+            return items;
+        } catch (Exception e) {
+            String message = String.format("Unable to query database to check user %s", authUserId);
+            throw new Exception(message, e);
+        } finally {
+            CloseConnection();
+        }
+    }
 }
